7468f7270a545e2af71744172f1e8116efb9b424
Omar Jaber <48085067+Ojay89@users.noreply.github.com>
2020-03-06 13:02:19 +0100

    Add project files.

---
+++ TCP-Server/Book.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCP_Server
+{
+    using System;
+
+    namespace SimpleRestService
+    {
+        public class Book
+        {
+            //Instance Field
+            private string _title;
+            private string _author;
+            private int _pageNo;
+            private string _isbn13;
+
+            //Constructor
+            public Book(string title, string author, int pageNo, string isbn13)
+            {
+                Title = title;
+                Author = author;
+                PageNo = pageNo;
+                Isbn13 = isbn13;
+            }
+
+            public Book()
+            {
+
+            }
+
+            //Properties
+            public string Title
+            {
+                get => _title;
+                set
+                {
+                    if (value == null) throw new NullReferenceException("Bogen skal have en titel");
+                    _title = value;
+                }
+            }
+
+            public string Author
+            {
+                get => _author;
+                set
+                {
+                    if (value == null) throw new NullReferenceException("Skriv et navn");
+                    if (value.Length < 2) throw new ArgumentException("Forfatter navn skal være minimum 2 tegn langt");
+                    _author = value;
+                }
+
+            }
+
+            public int PageNo
+            {
+                get => _pageNo;
+                set
+                {
+                    if (value <= 4) throw new ArgumentOutOfRangeException("Bogen er for kort");
+                    if (value >= 1000) throw new ArgumentOutOfRangeException("Bogen er for lang");
+                    _pageNo = value;
+                }
+            }
+
+            public string Isbn13
+            {
+                get => _isbn13;
+
+                set
+
+                {
+                    _isbn13 = value;
+                    if (value.Length == 13) _isbn13 = value;
+                    else if (value.Length != 13) throw new ArgumentException("ISBN skal være 13 cifre");
+
+                }
+            }
+        }
+    }
+}
+
+
+
---
+++ TCP-Server/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TCP_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Opret server
+            IPAddress ip = IPAddress.Parse("192.168.24.241");
+            //Opret adresse eller port
+            TcpListener serverSocket = new TcpListener(ip, 4646);
+            //starter server
+            serverSocket.Start();
+            Console.WriteLine("Server Started");
+
+            do
+            {
+                Task.Run(() =>
+                {
+                    //Venter på connection før den aktiverer
+                    TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                    Console.WriteLine("Server activated & Connected");
+                    //kalder metoden DoClient
+                    DoClient(connectionSocket);
+
+                });
+
+            } while (true);
+
+        }
+        public static void DoClient(TcpClient socket)
+        {
+            Stream ns = socket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+
+            string message = sr.ReadLine();
+            string answer = "";
+
+            while (message != null && message != "")
+            {
+                Console.WriteLine("Get all" + message);
+                answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+            }
+
+            ns.Close();
+            socket.Close();
+
+        }
+    }
+}
